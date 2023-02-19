402cf076c610b1fc93ddd5f87615ef65bae77d0f
Arkadii <arkadii.veretilin@niko-tech.eu>
2023-02-19 15:23:36 +0200

    Added validation to dto

--- src/common/dto/create-operation.dto.ts
+++ src/common/dto/create-operation.dto.ts
@@ -1,8 +1,17 @@
+import { IsNumber, IsEnum, IsOptional, IsString } from 'class-validator';
 import { OperationType } from '../enums';
 
 export class CreateOperationDto {
+  @IsNumber()
   amount: number;
+
+  @IsEnum(OperationType)
   type: OperationType;
+
+  @IsString()
   category: string;
+
+  @IsString()
+  @IsOptional()
   notes?: string;
 }
--- src/common/dto/update-operation.dto.ts
+++ src/common/dto/update-operation.dto.ts
@@ -1,5 +1,15 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class UpdateOperationDto {
+  @IsNumber()
+  @IsOptional()
   amount?: number;
+
+  @IsString()
+  @IsOptional()
   category?: string;
+
+  @IsString()
+  @IsOptional()
   notes?: string;
 }
